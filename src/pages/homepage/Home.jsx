6c23b4133fac8b65988de2b41ae55aa67f5df4c6
shrimon347 <shrimon1999@gmail.com>
2024-11-25 22:11:08 +0600

    shadcn ui setup

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <div className="text-3xl text-center p-10 text-green-500">This is Home</div>;
+  return (
+    <div className="text-3xl text-center p-10 text-green-500">
+      This is Home
+      <div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default Home;
