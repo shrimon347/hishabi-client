769bf33036e0a1918682d7cdb31b6bc25929f78d
shrimon347 <shrimon1999@gmail.com>
2024-11-26 20:36:26 +0600

    complete the login form

--- src/route/Route.jsx
+++ src/route/Route.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Error from "../components/shared/Error";
 import Home from "../pages/homepage/Home";
+import SignUp from "@/pages/authpage/SignUp";
+import Login from "@/pages/authpage/Login";
 
 const router = createBrowserRouter([
     {
@@ -12,7 +14,15 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />,
-            }
+            },
+            {
+                path: "/register",
+                element: <SignUp />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
         ]
     }
 ])
