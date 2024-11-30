15906e2c5a8c5349b918435052323265bf7eac20
Rimon Mridha <61982861+shrimon347@users.noreply.github.com>
2024-11-30 19:56:13 +0600

    Revert "Revert 5 feacture/dashbord"

--- src/route/Route.jsx
+++ src/route/Route.jsx
@@ -1,19 +1,39 @@
+import Login from "@/pages/authpage/Login";
+import SignUp from "@/pages/authpage/SignUp";
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layout/MainLayout";
 import Error from "../components/shared/Error";
+import MainLayout from "../layout/MainLayout";
 import Home from "../pages/homepage/Home";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <Error />,
+    children: [
+      {
         path: "/",
-        element: <MainLayout />,
-        errorElement : <Error />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            }
-        ]
-    }
-])
-export default router
+        element: <Home />,
+      },
+      {
+        path: "/register",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "/dashbord",
+    element: null,
+    children: [
+      {
+        path: "/dashbord",
+        element: null,
+      },
+    ],
+  },
+]);
+export default router;
