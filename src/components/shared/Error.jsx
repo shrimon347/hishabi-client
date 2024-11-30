55c8d2894dd57842d83136e5c66cbe353bc2ffbc
Rimon Mridha <61982861+shrimon347@users.noreply.github.com>
2024-11-30 19:47:34 +0600

    Revert "Feacture/dashbord"

--- src/components/shared/Error.jsx
+++ src/components/shared/Error.jsx
@@ -1,22 +1,13 @@
-/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
-import { Button } from "../ui/button";
 
 const Error = () => {
-
-  
   return (
     <div className="max-w-7xl mx-auto mt-[20%] text-center">
       <p className="text-6xl text-center font-extrabold text-green-900 ">
         404 Not Found
       </p>
-      <Link to="/dashboard">
-        <Button
-          variant="outline"
-          className="mt-10 border-green-500 hover:bg-green-100"
-        >
-          Back to home
-        </Button>
+      <Link to="/">
+        <button className="btn btn-primary mt-10">Back to home</button>
       </Link>
     </div>
   );
